9115869e820c08b991fdd7843343b3caab356cb7
Klervy <112544312+KatShulu@users.noreply.github.com>
2023-03-22 15:06:02 +0100

    the two dropdowns works miss css

--- src/components/GetCard.js
+++ src/components/GetCard.js
@@ -1,24 +1,23 @@
 //Get Card
 import React, { useState, useEffect } from "react";
 import Card from "./Card";
+import ButtonDesign from "./ButtonDesign";
 
 function GetCard(props) {
   const { urlToFetch } = props;
   //setter for fetch data
   const [data, setData] = useState([]);
   //setter for num of products to show
-  const[displayCount, setDisplayCount] = useState(9)
+  const [displayCount, setDisplayCount] = useState(9);
   //onclick gestion
-  const displayMore=()=>{
-    setDisplayCount(displayCount + 9)
-  }
+  const displayMore = () => {
+    setDisplayCount(displayCount + 9);
+  };
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(
-          urlToFetch
-        );
+        const response = await fetch(urlToFetch);
         const jsonData = await response.json();
 
         if (!response.ok) {
@@ -37,7 +36,7 @@ function GetCard(props) {
 
   return (
     <React.Fragment>
-      {data.slice(0, 9).map((item) => (
+      {data.slice(0, displayCount).map((item) => (
         <Card
           key={item.id}
           title={item.title}
@@ -47,6 +46,13 @@ function GetCard(props) {
           id={item.id}
         />
       ))}
+      <div className="btnSeeMore">
+        <ButtonDesign
+          onClick={displayMore}
+          color="secondary"
+          value="Display more"
+        />
+      </div>
     </React.Fragment>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,16 +5,37 @@ import HomeScrollDown from "../components/HomeScrollDown";
 import GetCard from "../components/GetCard";
 import ButtonDesign from "../components/ButtonDesign";
 import{ useState } from "react";
-import ColorDropdown from "../components/ColorDropDown";
+import ColorDropDownButton from "../components/ColorDropDownButton";
+import CategoryDropDownButton from "../components/CategoryDropDownButton";
+
 
 const Home = (props) => {
   const [urlToFetch, setUrlToFetch] = useState("http://127.0.0.1:8000/get_all_card_meuble")
 
+
     const handleSortPrice = () =>{
-      setUrlToFetch("http://127.0.0.1:8000/get_meuble_price")
+      switch (urlToFetch){
+        case "http://127.0.0.1:8000/get_all_card_meuble" :
+          setUrlToFetch("http://127.0.0.1:8000/get_meuble_price")
+        break
+        case "http://127.0.0.1:8000/get_meuble_price" :
+          setUrlToFetch("http://127.0.0.1:8000/get_all_card_meuble")
+        break
+        default : 
+          setUrlToFetch("http://127.0.0.1:8000/get_all_card_meuble")
+        break
+
+
+      }
     }
-    const handleSortColor = (color) =>{
-      setUrlToFetch(`http://127.0.0.1:8000/get_meuble_color/${color}`)
+
+    const handleSortColor = (selectedColor) =>{
+      console.log(selectedColor)
+      setUrlToFetch(`http://127.0.0.1:8000/get_meuble_color/${selectedColor}`)
+    }
+    const handleSortCategory = (selectedCategory) =>{
+      console.log(selectedCategory)
+      setUrlToFetch(`http://127.0.0.1:8000/get_meuble_category/${selectedCategory}`)
     }
 
     return (
@@ -26,7 +47,8 @@ const Home = (props) => {
           <div className="allProducts">
             <h1>Tous nos produits</h1>
             <ButtonDesign value="Classer par prix" color="secondary" onClick={handleSortPrice}></ButtonDesign>
-            <ColorDropdown value="Classer par couleur" color="secondary" onClick={() => handleSortColor("Noir")}></ColorDropdown>
+            <ColorDropDownButton onColorSelect={handleSortColor}></ColorDropDownButton> 
+            <CategoryDropDownButton onCategorySelect={handleSortCategory}></CategoryDropDownButton> 
 
           </div>
           <div className="cards">
