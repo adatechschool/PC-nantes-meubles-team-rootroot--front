3eb44d4e99103456f518dbd1acc7de6fba9d5e4c
Gaetan Sourisse <gaetansourisse@hotmail.fr>
2023-03-23 14:07:21 +0100

    "footer arrangé"

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
+import FooterFixed from '../components/FooterFixed';
 import TotalCart from '../components/TotalCart';
 import Adress from '../components/Adress';
 import GetProductCart from '../components/GetProductCart';
@@ -11,15 +11,17 @@ const Cart = () => {
     return (
         <>
         <Header/>
-        <div className='bodycartpage'>  
-            <img className='flower' src={flower} alt='fleur'/>
-            <div className='cartcontainer'>
-                {localStorage.cart ? (<GetProductCart /> ):(<span><strong>Votre panier est vide!</strong></span>)}
-                {/* <TotalCart price="79"/> */}
-                 {/* <Adress/> */}
-            </div>
-        </div> 
-        <Footer/>
+        <div className='container'>
+            <div className='bodycartpage'>  
+                <img className='flower' src={flower} alt='fleur'/>
+                <div className='cartcontainer'>
+                    {localStorage.cart ? (<GetProductCart /> ):(<span><strong>Votre panier est vide!</strong></span>)}
+                    {/* <TotalCart price="79"/> */}
+                    {/* <Adress/> */}
+                </div>
+            </div> 
+        </div>
+        <FooterFixed/>
         </>
     );
 }
